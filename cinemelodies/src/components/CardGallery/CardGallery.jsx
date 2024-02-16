3658e900fe2cf7f84c00a247acb977b14c04f406
Jonathon E <je20k3@gmail.com>
2024-02-16 12:16:05 +0000

    Feat: Add CardGallery component & basic styling

---
+++ cinemelodies/src/components/CardGallery/CardGallery.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import "./CardGallery.css";
+
+const CardGallery = () => {
+  return (
+      <div class="gallery-container">CardGallery (movie cards go inside here)
+       </div>
+  )
+}
+
+export default CardGallery;
