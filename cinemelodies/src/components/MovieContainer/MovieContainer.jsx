fe779f19e8ae043e725d0bf48036cef340df80f2
Jonathon E <je20k3@gmail.com>
2024-02-19 18:40:32 +0000

    Style: Modify MovieContainer for improved layout and styling

--- cinemelodies/src/components/MovieContainer/MovieContainer.jsx
+++ cinemelodies/src/components/MovieContainer/MovieContainer.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import fetchMovieData from "../../Api"; // Assuming Api.js is in the same directory
+import fetchMovieData from "../../Api";
 import Movie from "../Movie/Movie";
 import "./MovieContainer.css";
 
@@ -17,7 +17,7 @@ const MovieContainer = () => {
         const movieData = await fetchMovieData();
         setMovies(movieData);
       } catch (error) {
-        console.error("Error fetching movie data:", error);
+        console.error("Error getting movie info:", error);
       }
     };
 
@@ -26,86 +26,99 @@ const MovieContainer = () => {
 
   return (
     <>
-    <div className="moviesContainer">
-    <div>
-      <h2 className="text-center text-2xl font-bold mb-4">Comedies</h2>
-      <div className="movieList">
-        {movies.comedy.map((movie) => (
-          <div key={movie.id} className="movieCard">
-            <h3>{movie.title}</h3>
-            <img
-              src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-              alt={movie.title}
-              className="posterImage"
-            />
-            <p className="text-center font-bold mb-4">Release Date: {movie.release_date}</p>
-            <p className="movieDescription">{movie.overview}</p>
+      <div className="moviesContainer">
+        <div>
+          <h2 className="text-left text-2xl font-bold mb-4">Comedy</h2>
+          <p className="moviesContainer-description text-left">
+            Top comedy movies that sound as good as they look
+          </p>
+          <div className="movieList">
+            {movies.comedy.map((movie) => (
+              <div key={movie.id} className="movieCard">
+                <img
+                  src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                  alt={movie.title}
+                  className="posterImage"
+                />
+                <span className="movieDate font-bold mb-4">
+                 {parseFloat(movie.release_date).toFixed(0)}
+                </span>
+                
+              </div>
+            ))}
           </div>
-        ))}
-      </div>
-    </div>
-    </div>
-    
-    <div className="moviesContainer">  
-      <div>
-      <h2 className= "text-center text-2xl font-bold mb-4">Fantasy</h2>
-      <div className="movieList">
-      {movies.fantasy.map((movie) => (
-        <div key={movie.id} className="movieCard">
-        <h3>{movie.title}</h3>
-        <img
-          src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-          alt={movie.title}
-          className="posterImage"
-        />
-        <p className="text-center font-bold mb-4">Release Date: {movie.release_date}</p>
-        <p className="movieDescription">{movie.overview}</p>
-      </div>
-    ))}
-  </div>
-    </div>
         </div>
+      </div>
 
-      <div className="moviesContainer">  
-      <div>
-      <h2 className="text-center text-2xl font-bold mb-4">Magic</h2>
-      <div className="movieList">
-      {movies.magic.map((movie) => (
-       <div key={movie.id} className="movieCard">
-       <h3>{movie.title}</h3>
-       <img
-         src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-         alt={movie.title}
-         className="posterImage"
-       />
-       <p className="text-center font-bold mb-4">Release Date: {movie.release_date}</p>
-       <p className="movieDescription">{movie.overview}</p>
-     </div>
-   ))}
- </div>
-</div>
-</div>
-<div className="moviesContainer">
-<div>
-      <h2 className= "text-center text-2xl font-bold mb-4">Recommended</h2>
-      <div className="movieList">
-      {movies.recommended.map((movie) => (
-       <div key={movie.id} className="movieCard">
-       <h3>{movie.title}</h3>
-       <img
-         src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-         alt={movie.title}
-         className="posterImage"
-       />
-       <p className="text-center font-bold mb-4">Release Date: {movie.release_date}</p>
-       <p className="movieDescription">{movie.overview}</p>
-     </div>
-   ))}
- </div>
-</div>
-</div>
-</>
-)
+      <div className="moviesContainer">
+        <div>
+          <h2 className="text-left text-2xl font-bold mb-4">Fantasy</h2>
+          <p className="moviesContainer-description text-left">
+            Enter a world of fantastical esapism with one of these otherworldy
+            classics
+          </p>
+          <div className="movieList">
+            {movies.fantasy.map((movie) => (
+              <div key={movie.id} className="movieCard">
+                <img
+                  src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                  alt={movie.title}
+                  className="posterImage"
+                />
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+      <div className="moviesContainer">
+        <div>
+          <h2 className="text-left text-2xl font-bold mb-4">Magic</h2>
+          <p className="moviesContainer-description text-left">
+            Manifest some movie magic
+          </p>
+          <div className="movieList">
+            {movies.magic.map((movie) => (
+              <div key={movie.id} className="movieCard">
+                <img
+                  src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                  alt={movie.title}
+                  className="posterImage"
+                />
+                <p className="text-center font-bold mb-4">
+                  Release Date: {movie.release_date}
+                </p>
+                <p className="movieDescription">{movie.overview}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+      <div className="moviesContainer">
+        <div>
+          <h2 className="text-left text-2xl font-bold mb-4">Team Favourites</h2>
+          <p className="moviesContainer-description text-left">
+            A curated collection of the CineMelodies team's favourites - all
+            with exceptional soundtracks.
+          </p>
+          <div className="movieList">
+            {movies.recommended.map((movie) => (
+              <div key={movie.id} className="movieCard">
+                <img
+                  src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                  alt={movie.title}
+                  className="posterImage"
+                />
+                <p className="text-center font-bold mb-4">
+                  Release Date: {movie.release_date}
+                </p>
+                <p className="movieDescription">{movie.overview}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default MovieContainer;
