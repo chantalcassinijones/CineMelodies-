1ca5b6f7b04941cb47ceefe6e138dfbbe89372b5
Jonathon E <je20k3@gmail.com>
2024-02-21 09:21:38 +0000

    Fix: Added components to Header + styling

--- cinemelodies/src/components/Hero/Hero.jsx
+++ cinemelodies/src/components/Hero/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
     
    <div className="hero-container text-center">
      <h1 className="font-bold tracking-tight pt-5 sm:text-4xl">
-     Your guide to the world of movies and sounds
+     Your guide to the world of sounds and movies
      </h1>
      <div className="mt-10 flex items-center justify-center gap-x-6">
       <MovieSearch />
