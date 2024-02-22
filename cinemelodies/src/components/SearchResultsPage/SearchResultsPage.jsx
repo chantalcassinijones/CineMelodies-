dc893b0d05e65b892ee3a6bd4c241ab98879ad54
Jonathon E <je20k3@gmail.com>
2024-02-22 11:52:16 +0000

    Styling: refactor MovieInfo/SoundInfo styling

--- cinemelodies/src/components/SearchResultsPage/SearchResultsPage.jsx
+++ cinemelodies/src/components/SearchResultsPage/SearchResultsPage.jsx
@@ -5,26 +5,35 @@ import "../MovieContainer/MovieContainer.css";
 
 // Render results to container
 const SearchResultsPage = ({ movies }) => {
-  
   // Return only first 8 movies with highest user votes
-    const selectedMovies = movies? movies.sort((a, b) => b.popularity - a.popularity).slice(0, 8): "";
-      
+  const selectedMovies = movies
+    ? movies.sort((a, b) => b.popularity - a.popularity).slice(0, 8)
+    : "";
 
   return (
     <div className="moviesContainer">
-         
-          {selectedMovies.length === 0 ? <h2 className="noresults-h2">No results found! Why not try a different query?</h2> : (<>
-               <h2 className="text-left text-2xl font-bold">Top search results</h2>
-              <div className="movieList">
-                  {selectedMovies.map((movie) => (
-                      <Movie
-                          key={movie.id}
-                          title={movie.title}
-                          releaseDate={movie.release_date}
-                          posterPath={movie.poster_path}
-                      />
-                  ))}
-              </div></>) }
+      {selectedMovies.length === 0 ? (
+        <h2 className="noresults-h2">
+          No results found! Why not try a different query?
+        </h2>
+      ) : (
+        <div className="searches">
+          <h2 className="text-left text-2xl font-bold">Top search results</h2>
+          <div className="movieList">
+            {selectedMovies.map((movie) => (
+              <Movie
+                key={movie.id}
+                title={movie.title}
+                releaseDate={movie.release_date}
+                posterPath={movie.poster_path}
+                backdropPath={movie.backdrop_path}
+                voteAverage={movie.vote_average}
+                overview={movie.overview}
+              />
+            ))}
+          </div>
+        </div>
+      )}
     </div>
   );
 };
