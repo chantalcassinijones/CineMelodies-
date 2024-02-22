91eed4cced316be008842bc462107bfcfcceed06
Chantal Cassini-Jones <ccassinijones@gmail.com>
2024-02-22 22:28:09 +0000

    Refactor: navbar logo add

--- cinemelodies/src/components/Navbar/Navbar.jsx
+++ cinemelodies/src/components/Navbar/Navbar.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import "./Navbar.css"
 import { Link } from 'react-router-dom';
-
+import Logo from "./logo.png"
 
 const Navbar = () => {
   return (
     <nav className="navbar">
     <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
       <div className="flex items-center h-16">
-        <div className="flex-shrink-0">
+        <div className= " flex-shrink-0 flex items-center">
+          <img className=" w-16 p-0 self-center " src={Logo} />
           <span className="text-white font-bold text-lg">CineMelodies</span>
         </div>
         <div className="hidden md:block">
