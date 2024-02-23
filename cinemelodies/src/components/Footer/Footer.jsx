fbdf30ce4071da11eeac60293648db40f289d438
Jonathon E <je20k3@gmail.com>
2024-02-23 16:22:16 +0000

    Refactor: Adjust Footer text

--- cinemelodies/src/components/Footer/Footer.jsx
+++ cinemelodies/src/components/Footer/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
     <footer className="footer-container bg-yellow-700">
       <div className="container mx-auto py-4 px-6">
         <h1><p className="text-center text-white">
-          The Ternary Operators 2024 
+          CineMelodies MVP by The Ternary Operators 2024 
         </p></h1>
       </div>
     </footer>
